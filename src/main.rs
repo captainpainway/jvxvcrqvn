f68cc7b0e0751878a2bf29188411e2488845e6a3
Mary Knize <maryknize@gmail.com>
2020-11-07 10:12:19 -0500

    Fetching page text

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 extern crate iron;
 extern crate router;
 extern crate reqwest;
+extern crate serde_json;
+extern crate percent_encoding;
 
 use std::path::Path;
 use iron::prelude::*;
 use iron::status;
 use router::Router;
+use serde_json::Value;
+use percent_encoding::{utf8_percent_encode, AsciiSet, CONTROLS};
 
 fn main() {
     let mut router = Router::new();
@@ -25,8 +29,16 @@ fn random_handler(_: &mut Request) -> IronResult<Response> {
 }
 
 fn get_json() -> Result<String, reqwest::Error> {
-    let url = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=random&grnnamespace=0&grnlimit=1";
-    let json = reqwest::blocking::get(url)?.text()?;
-    println!("{:?}", json);
-    Ok(json)
+    let url = "https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=1&format=json";
+    let json: Value = reqwest::blocking::get(url)?.json()?;
+    let title = json["query"]["random"][0]["title"].as_str().unwrap();
+
+    const FRAGMENT: &AsciiSet = &CONTROLS.add(b' ').add(b'(').add(b')');
+    let encoded_title = utf8_percent_encode(title, FRAGMENT).to_string();
+
+    let page_url = "https://en.wikipedia.org/w/api.php?action=parse&page=".to_owned() + &encoded_title + "&prop=text&format=json";
+    let page_json: Value = reqwest::blocking::get(&page_url)?.json()?;
+    let text = &page_json["parse"]["text"]["*"].as_str().unwrap();
+
+    Ok(text.to_string())
 }
